3322faa19e5d10efcc139ea46b9cdaa1b604c9fb
Karie Funk <kfunk@my365.bellevue.edu>
2019-06-03 14:30:52 -0600

    variablesNaming

---
+++ week-1/Karie-Funk.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+var firstName;
+
+firstName ="Karie";
+
+console.log(firstName);
+
+
+
+var lastName;
+
+lastName = "Funk";
+
+console.log(lastName);
+
+
+
+var age;
+
+age = "24";
+
+console.log(age);
+
+
+
+var street;
+
+street = "126 Zion Ln";
+
+console.log(street);
+
+
+
+var city;
+
+city = "Austin";
+
+console.log(city);
+
+
+
+var zipCode;
+
+zipCode = "69001";
+
+console.log(zipCode);
+
+
+

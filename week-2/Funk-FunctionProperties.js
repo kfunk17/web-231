9055b7dc5d5cb596f7c0d0fe25c825fac0a14184
Karie Funk <kfunk@my365.bellevue.edu>
2019-06-09 19:18:43 -0600

    update

---
+++ week-2/Funk-FunctionProperties.js
@@ -0,0 +1,32 @@
+
+
+/*
+============================================
+; Title:  Assignment 2.3 Function Properties
+; Author: Professor Krasso 
+; Date:   09 June 2019
+; Modified by: Karie Funk
+; Description: Creating a function property
+;===========================================
+*/
+
+var header = require('./Funk-header.js');
+
+console.log(header.display("Karie", "Funk", "Week 2"));
+
+
+
+// Function properties go here
+
+myName.Karie = "Karie";
+
+// Function
+
+function myName() {
+  return myName.Karie;
+}
+
+// Output
+console.log('\n');
+console.log("Hello " + myName() + " Funk");
+
